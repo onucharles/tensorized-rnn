e05b6e83db7dd4045b651b1e53cd0848d07f87d7
Charles C Onu <onucharlesc@gmail.com>
2020-06-02 08:57:47 -0400

    fixed lstm inheritance issue.

--- encoder/models/lstm.py
+++ encoder/models/lstm.py
@@ -9,8 +9,14 @@ class LSTMCell(nn.Module):
         self.hidden_size = hidden_size
         self.bias = bias
         self.device = device
-        self.input_weights = nn.Linear(input_size, 4 * hidden_size, bias).to(device)
-        self.hidden_weights = nn.Linear(hidden_size, 4 * hidden_size, bias).to(device)
+        self.input_weights = self._create_input_hidden_weights()
+        self.hidden_weights = self._create_hidden_hidden_weights()
+
+    def _create_input_hidden_weights(self):
+        return nn.Linear(self.input_size, 4 * self.hidden_size, self.bias).to(self.device)
+
+    def _create_hidden_hidden_weights(self):
+        return nn.Linear(self.hidden_size, 4 * self.hidden_size, self.bias).to(self.device)
 
     def init_hidden(self, batch_size, hidden_size):
         h = torch.zeros(batch_size, hidden_size).to(self.device)
@@ -39,17 +45,24 @@ class LSTM(nn.Module):
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         self.bias = bias
+        self.device = device
 
         # instantiate lstm cell for each layer.
         self._all_layers = []
         for i in range(self.num_layers):
             name = 'cell{}'.format(i)
             if i == 0:
-                cell = LSTMCell(self.input_size, self.hidden_size, self.bias, device)
+                cell = self._create_first_layer_cell()
             else:
-                cell = LSTMCell(self.hidden_size, self.hidden_size, self.bias, device)
+                cell = self._create_other_layer_cell()
             setattr(self, name, cell)
             self._all_layers.append(cell)
+
+    def _create_first_layer_cell(self):
+        return LSTMCell(self.input_size, self.hidden_size, self.bias, self.device)
+
+    def _create_other_layer_cell(self):
+        return LSTMCell(self.hidden_size, self.hidden_size, self.bias, self.device)
 
     def forward(self, input):
         # input: (batch_size, n_steps, input_size)
--- encoder/models/tt_lstm.py
+++ encoder/models/tt_lstm.py
@@ -1,39 +1,36 @@
-from torch import nn
 from .lstm import LSTMCell, LSTM
 from t3nsor.layers import TTLinear
 
+
 class TTLSTMCell(LSTMCell):
     def __init__(self, input_size, hidden_size, bias, device, n_cores, tt_rank):
+        self.n_cores = n_cores
+        self.tt_rank = tt_rank
         super().__init__(input_size, hidden_size, bias, device)
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-        self.bias = bias
-        self.device = device
-        self.input_weights = TTLinear(in_features=input_size, out_features=4 * hidden_size,
-                            bias=bias, auto_shapes=True, d=n_cores, tt_rank=tt_rank).to(device)
-        self.hidden_weights = TTLinear(in_features=hidden_size, out_features=4 * hidden_size,
-                            bias=bias, auto_shapes=True, d=n_cores, tt_rank=tt_rank).to(device)
-        #self.input_weights = nn.Linear(input_size, 4 * hidden_size, bias).to(device)
-        #self.hidden_weights = nn.Linear(hidden_size, 4 * hidden_size, bias).to(device)
+
+    def _create_input_hidden_weights(self):
+        return TTLinear(in_features=self.input_size, out_features=4 * self.hidden_size,
+                        bias=self.bias, auto_shapes=True, d=self.n_cores,
+                        tt_rank=self.tt_rank).to(self.device)
+
+    def _create_hidden_hidden_weights(self):
+        return TTLinear(in_features=self.hidden_size, out_features=4 * self.hidden_size,
+                        bias=self.bias, auto_shapes=True, d=self.n_cores,
+                        tt_rank=self.tt_rank).to(self.device)
 
 
 class TTLSTM(LSTM):
     def __init__(self, input_size, hidden_size, num_layers, device, bias, n_cores, tt_rank):
+        self.n_cores = n_cores
+        self.tt_rank = tt_rank
         super().__init__(input_size, hidden_size, num_layers, device, bias)
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-        self.num_layers = num_layers
-        self.bias = bias
 
-        # instantiate lstm cell for each layer.
-        self._all_layers = []
-        for i in range(self.num_layers):
-            name = 'cell{}'.format(i)
-            if i == 0:
-                cell = TTLSTMCell(self.input_size, self.hidden_size, self.bias, device,
-                                n_cores=n_cores, tt_rank=tt_rank)
-            else:
-                cell = TTLSTMCell(self.hidden_size, self.hidden_size, self.bias, device,
-                                n_cores=n_cores, tt_rank=tt_rank)
-            setattr(self, name, cell)
-            self._all_layers.append(cell)
+    def _create_first_layer_cell(self):
+        return TTLSTMCell(self.input_size, self.hidden_size, self.bias, self.device,
+                          n_cores=self.n_cores, tt_rank=self.tt_rank)
+
+    def _create_other_layer_cell(self):
+        return TTLSTMCell(self.hidden_size, self.hidden_size, self.bias, self.device,
+                          n_cores=self.n_cores, tt_rank=self.tt_rank)
+
+
