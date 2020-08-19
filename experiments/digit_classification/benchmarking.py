5f927c42e1842443bfa3a40c98e7dcf4fc6d7e56
Charles C Onu <onucharlesc@gmail.com>
2020-08-19 12:57:30 -0400

    Implemented generic benchmarking.

--- experiments/digit_classification/benchmarking.py
+++ experiments/digit_classification/benchmarking.py
@@ -3,54 +3,26 @@ Benchmarking runtime of digit classification models.
 """
 
 import torch
+import torch.optim as optim
+import torch.nn.functional as F
 import argparse
 from time import time
 import numpy as np
 from mnist_classifier import MNIST_Classifier
 from GPUtil import showUtilization as gpu_usage
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--tt", action='store_true')
-    parser.add_argument("--ncores", type=int, default=1)
-    parser.add_argument("--ttrank", type=int, default=1)
-    parser.add_argument("--gru", action='store_true')
-    parser.add_argument("--permute", action='store_true')
-    parser.add_argument('--batch_size', type=int, default=128, metavar='N')
-    parser.add_argument('--hidden_size', type=int, default=256)
-    parser.add_argument('--n_layers', type=int, default=1)
-    parser.add_argument("-n", "--nruns", type=int, default=100)
-    args = parser.parse_args()
-    print(args)
 
-    if args.permute:  # each pixel is input for a time step.
-        input_channels = 1
-    else:  # each row(of pixels) is input for a time step.
-        input_channels = 28
-    n_classes = 10
-    seq_length = int(784 / input_channels)
-
-    # instantiate model
-    device = torch.device('cuda')
-    model = MNIST_Classifier(input_channels, n_classes, args.hidden_size, args.n_layers, device,
-                             tt=args.tt, gru=args.gru, n_cores=args.ncores,
-                             tt_rank=args.ttrank).to(device)
-
-    # create random batch of data (using appropriate sizes)
-    random_batch = np.random.rand(args.batch_size, seq_length, input_channels).astype('float32')
-    random_batch = torch.from_numpy(random_batch).to(device)
-    print("Benchmarking with input of shape (batch_size, seq_length, input_channels): ",
-          random_batch.shape)
-
-    # run and time forward pass x times.
+def testing_eval():
+    print("Benchmarking test time...")
     model.eval()
     all_durations = []
-    out = model(random_batch)   # do one time to wake gpu.
+    _ = model(data)   # do one time to wake gpu.
+
     with torch.no_grad():
         for i in np.arange(args.nruns):
             # time forward pass
             start = time()
-            out = model(random_batch)
+            _ = model(data)
             gpu_usage()
             duration = time() - start
 
@@ -63,3 +35,81 @@ if __name__ == '__main__':
     mean_time = np.mean(all_durations)
     std_time = np.std(all_durations)
     print(f"mean time: {mean_time} \t std time: {std_time}")
+
+
+def training_eval():
+    print("Benchmarking training time...")
+    optimizer = getattr(optim, 'Adam')(model.parameters(), lr=0.001)
+    model.train()
+    all_durations = []
+    out = model(data)   # do one time to wake gpu.
+
+    for i in np.arange(args.nruns):
+        # start timer
+        start = time()
+
+        optimizer.zero_grad()
+        out = model(data)
+        loss = F.nll_loss(out, target)
+        loss.backward()
+        optimizer.step()
+
+        # end timer.
+        gpu_usage()
+        duration = time() - start
+
+        # print and save duration
+        print(f"Run: {i} \t Duration: {duration}", )
+        all_durations.append(duration)
+
+    # print mean and std of durations.
+    all_durations = np.array(all_durations)
+    mean_time = np.mean(all_durations)
+    std_time = np.std(all_durations)
+    print(f"mean time: {mean_time} \t std time: {std_time}")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--tt", action='store_true')
+    parser.add_argument("--ncores", type=int, default=1)
+    parser.add_argument("--ttrank", type=int, default=1)
+    parser.add_argument('--batch_size', type=int, default=512, metavar='N')
+    parser.add_argument("--in_size", type=int, default=256)
+    parser.add_argument('--hidden_size', type=int, default=512)
+    parser.add_argument('--n_layers', type=int, default=1)
+    parser.add_argument("--seq_len", type=int, default=160)
+    parser.add_argument("--emb_size", type=int, default=256)
+    parser.add_argument("-n", "--nruns", type=int, default=100)
+    parser.add_argument("--gru", action='store_true')
+    parser.add_argument("--naive_tt", action='store_true')
+    parser.add_argument("--train", action='store_true')
+    parser.add_argument("--cuda", action='store_true')
+    args = parser.parse_args()
+    print(args)
+
+    # instantiate model
+    if args.cuda:
+        device = torch.device('cuda')
+    else:
+        device = torch.device('cpu')
+
+    # Using the MNIST_Classifier class, but it's just a generic RNN with fully connected layer.
+    model = MNIST_Classifier(args.in_size, args.emb_size, args.hidden_size, args.n_layers, device,
+                             tt=args.tt, gru=args.gru, n_cores=args.ncores, tt_rank=args.ttrank,
+                             naive_tt=args.naive_tt).to(device)
+
+    # create random batch of data (using appropriate sizes)
+    data = np.random.rand(args.batch_size, args.seq_len, args.in_size).astype('float32')
+    target = np.random.randint(0, args.emb_size, args.batch_size).astype('int64')
+    data = torch.from_numpy(data).to(device)
+    target = torch.from_numpy(target).to(device)
+    print("Benchmarking with input: {} and target {}".format(data.size(), target.shape))
+
+    if args.train:
+        training_eval()
+    else:
+        testing_eval()
+
+    print(f"model has: {model.param_count()} parameters")
+
